fb76e6a9fdc460cb194a66c8969e3196b7afc342
Felipe-Borba <felipe.borba0109@gmail.com>
2022-05-12 21:26:12 -0300

    create spring project

---
+++ src/main/java/com/borba/backendprovasenior/BackendProvaSeniorApplication.java
@@ -0,0 +1,13 @@
+package com.borba.backendprovasenior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProvaSeniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendProvaSeniorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/borba/backendprovasenior/BackendProvaSeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borba.backendprovasenior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProvaSeniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
